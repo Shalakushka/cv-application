5c1e686daa6311cfc7e6e0714ec42420b2c4d040
Shalakushka <56279749+Shalakushka@users.noreply.github.com>
2024-07-15 13:09:39 +0200

    Included Roboto fonts and a header in the App JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
+import './style/App.css'
 
 function App() {
   return (
-    <></>
+    <>
+    <header>
+      <h1>CV Maker</h1>
+    </header>
+    </>
   )
 }
 
